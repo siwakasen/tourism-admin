03247ee44d72c3db20482075b63d258cf4bcbfdd
siwakasen <siwakasen@gmail.com>
2025-04-15 16:03:15 +0700

    update packages and build vite

--- vite.config.ts
+++ vite.config.ts
@@ -14,4 +14,22 @@ export default defineConfig({
     },
     extensions: [".js", ".ts", ".jsx", ".tsx"],
   },
+  build: {
+    minify: 'esbuild',
+    rollupOptions:{
+        output: {
+            manualChunks(id) {
+            if (id.includes("node_modules")) {
+                return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+            },
+
+        },
+
+    }
+}
 });
