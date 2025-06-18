04e74b43aa025ef5195f7c4c77b962190644f380
siwakasen <siwakasen@gmail.com>
2025-06-18 14:53:38 +0000

    Edit constant.ts

--- src/_constants/constant.ts
+++ src/_constants/constant.ts
@@ -1,4 +1,4 @@
-export const VITE_APP_REST_HOST = "https://tour.cashtrack.my.id";
-export const VITE_APP_REST_AUTH = "https://auth.cashtrack.my.id";
-export const VITE_APP_REST_TESTI = "https://testi.cashtrack.my.id";
-export const VITE_APP_REST_DRIVERS = "https://drivers.cashtrack.my.id";
+export const VITE_APP_REST_HOST = "https://tour.vulpbox.com";
+export const VITE_APP_REST_AUTH = "https://auth.vulpbox.com";
+export const VITE_APP_REST_TESTI = "https://testi.vulpbox.com";
+export const VITE_APP_REST_DRIVERS = "https://drivers.vulpbox.com";
